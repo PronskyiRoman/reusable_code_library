59604b678e6d66ff7428d8b079303bfd54cde110
Roman-Pronskyi <Roman. Pro, [21.10.21 09:26]wildnoblebrute@gmail.com>
2021-12-13 14:43:17 +0200

    clear project init

---
+++ reuseCodeLibrary/reuseCodeLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reuseCodeLibrary
+//
+//  Created by roman.pronskiy on 13.12.2021.
+//  Copyright © 2021 roman.pronskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
